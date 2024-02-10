a6aeec2c9b02bf96716a96cda2d64ad665286994
Zioun <jionkhan0@gmail.com>
2024-02-10 15:39:34 +0600

    added

---
+++ js/dom.js
@@ -0,0 +1,21 @@
+// ! 1.get element by tagname;
+const liCollection = document.getElementsByTagName("li");
+for(const li of liCollection){
+    console.log(li.innerText);
+}
+// ! 2.get element by id;
+const h1Tag = document.getElementById("title");
+console.log(h1Tag.innerText)
+// ! 3.get element by classname; (for of)
+const sp = document.getElementsByClassName("sp");
+for(let item of sp){
+    console.log(item.innerText);
+}
+// ! 4. query selector;
+const qury = document.querySelector('#title');
+console.log(qury.innerText);
+// ! 5. query selectorAll; (forEach)
+const quryAll = document.querySelectorAll('#title');
+for(let item of quryAll){
+    console.log(item.innerText)
+}
